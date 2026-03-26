39a608e24934c7c22f28b34b38dfc770b22546a9
3nz0k <e.mrtnez7@gmail.com>
2026-03-26 11:01:46 +0100

    Enhance styling and functionality for dark mode and settings menu in isoloir

--- js/options.js
+++ js/options.js
@@ -1,3 +1,29 @@
 function Options() {
-    console.log("Tu viens de cliquer sur le bouton Option !");
+    const div = document.getElementById("menu");
+    div.classList.toggle("hidden");
+}
+
+function Dark()
+{
+    const body = document.getElementById("body");
+    const header = document.getElementById("header");
+    const divs = document.querySelectorAll(".settings div");
+    const session = document.getElementById("session");
+    const item = document.getElementById("item");
+    const card1 = document.getElementById("card1");
+    const card2 = document.getElementById("card2");
+    const progress = document.getElementById("progress");
+    
+    console.log(divs);
+
+    body.classList.toggle("body-dark");
+    header.classList.toggle("header-dark");
+    session.classList.toggle("session-dark");
+    item.classList.toggle("item-dark");
+    card1.classList.toggle("card-dark");
+    card2.classList.toggle("card-dark");
+    progress.classList.toggle("progress-dark");
+    divs.forEach((div, index) => {
+        div.classList.toggle(index % 2 === 0 ? "settings-div-odd-dark" : "settings-div-even-dark");
+    });
 }
