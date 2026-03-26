5160132e0d8f64233b25f8d8cd87c2887ff3e4ab
3nz0k <e.mrtnez7@gmail.com>
2026-03-26 16:52:08 +0100

    Add modal functionality and styles for adding candidates; update HTML structure and styles

---
+++ js/accueil.js
@@ -0,0 +1,23 @@
+function Add() {
+    const modal = document.getElementById("modalAdd");
+
+    modal.classList.remove("hidden");
+}
+
+function Close() {
+    const modal = document.getElementById("modalAdd");
+    modal.classList.add("hidden");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const lastName = form.lastName.value;
+        const firstName = form.firstName.value;
+
+        console.log("lastName :", lastName);
+        console.log("firstName :", firstName);
+    });
+});
