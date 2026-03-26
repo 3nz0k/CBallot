6728dd5ced61ada198594711081dcec6237dbec8
Sauvraign <sauvraign.40@gmail.com>
2026-03-26 11:25:33 +0100

    Proposition structure JSon pour module isoloir

--- js/passer.js
+++ js/passer.js
@@ -1,3 +1,27 @@
+
+const candidates = [
+    {
+        "representative": {
+            "firstName": "Louis",
+            "pic": "photo.png"
+        },
+        "alternate": {
+            "firstName": "Emma",
+            "pic1": "photo.png"
+        }
+    },
+    {
+        "representative": {
+            "firstName": "Bob",
+            "pic": "photo.png"
+        },
+        "alternate": {
+            "firstName": "Bobby",
+            "pic1": "photo.png"
+        }
+    }
+];
+
 function Passer() {
     console.log("Tu viens de passer ton vote !");
 }
